6772e092534881463c74eb7210fb18cc12bf2883
Marcel Betański <aplay58.yt@gmail.com>
2026-02-12 12:44:13 +0100

    try to get some weather data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { useQuery } from '@tanstack/react-query';
+import { getWeatherAt } from './utils/api';
+
 const App = () => {
-  return <div className='text-4xl'>Hello, World!</div>;
+  const { data } = useQuery({
+    queryKey: ['weather'],
+    queryFn: () => getWeatherAt({ lat: 50, lon: 50 }),
+  });
+
+  return (
+    <main className='p-4'>
+      <div className='text-4xl font-bold'>{JSON.stringify(data)}</div>
+    </main>
+  );
 };
 
 export default App;
